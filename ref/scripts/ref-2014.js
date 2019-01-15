a919dc752171672c3170fadef52a3f49ffde2966
Alex <alex@rosemary.local>
2019-01-15 22:28:49 +0000

    update

---
+++ ref/scripts/ref-2014.js
@@ -0,0 +1,9 @@
+// Labels along the x-axis
+var levels = ["4*", "3*", "2*", "1*", "U/C"];
+
+// Quality scores
+var overall = [15, 47, 36, 2, 0];
+var Outputs = [10.5, 47.6, 38.7, 3.2, 0];
+var Impact = [40, 50, 10, 0, 0];
+var Environment = [0, 4, 6, 0, 0];
+
