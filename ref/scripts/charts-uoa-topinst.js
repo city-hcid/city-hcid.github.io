10e2acb6ebf9ed10eaadecd096a7f806aa6ffd04
Alex <alex@rosemary.local>
2019-04-02 22:47:50 +0100

    update

--- ref/scripts/charts-uoa-topinst.js
+++ ref/scripts/charts-uoa-topinst.js
@@ -28,7 +28,7 @@ var app = new Vue({
                     self.items = response.data.records;
                     topItem = self.items;
                     console.log(self.items);
-                    console.log(obj[0].fields['inst'])
+                    console.log(topItem[0])
                 })
                 .catch(function(error) {
                     console.log(error)
