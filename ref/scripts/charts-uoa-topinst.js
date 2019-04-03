da578c998da4a95e97c6102c493cdceefc453ac0
Alex <alex@rosemary.local>
2019-04-03 21:12:11 +0100

    update

--- ref/scripts/charts-uoa-topinst.js
+++ ref/scripts/charts-uoa-topinst.js
@@ -36,7 +36,8 @@ var app = new Vue({
                         eval(obj[i].fields['Profile'] + obj[i].fields['code'] + ' = ' + obj[i].fields['array'] + ";");
                         //Set chart dataset for each entry
                         eval('var ' + obj[i].fields['Profile'] + obj[i].fields['code'] + 'Dataset  = { data: ' + obj[i].fields['Profile'] + obj[i].fields['code'] + ',' + 'backgroundColor: backgroundColor' + obj[i].fields['Profile'] + ', borderColor: col_array, borderWidth: 1 }');
-                        console.log(obj[j].fields['inst'] + " " + obj[i].fields['Profile'])
+                        console.log(obj[i].fields['inst'] + " " + obj[i].fields['Profile'])
+                        var b = obj[i].fields['inst'] + obj[i].fields['uoa'] + obj[i].fields['Profile'] + '_' + obj[i].fields['year'];
                     }
 
 
