7b81bc3eedc508e6618c54578c94b3b6bddfa8a3
Alex <alex@rosemary.local>
2019-04-03 23:09:23 +0100

    update

--- ref/scripts/charts-uoa-topinst.js
+++ ref/scripts/charts-uoa-topinst.js
@@ -71,6 +71,19 @@ var app = new Vue({
 
                                 '</div>\n');
                         }
+                        a = document.getElementById(b);
+                        if (a !== null) {
+                            this.b = new Chart(a, {
+                                type: 'polarArea',
+                                data: {
+                                    labels: levels,
+                                    datasets: [
+                                        eval(c)
+                                    ]
+                                },
+                                options: polarOptionsC
+                            });
+                        }
                     }
                 })
                 .catch(function(error) {
