a80126ad420f5f9bf1bd3b315da30845501f2999
Alex <alex@rosemary.local>
2020-05-20 08:07:39 +0100

    update

--- interactions/js/vue_component.js
+++ interactions/js/vue_component.js
@@ -4,11 +4,12 @@ Vue.component('table-head', {
 })
 
 Vue.component('author-details', {
-    props: ['name', 'content'],
+    props: ['name', 'type', 'content'],
     template: `<div class="my-2">
-    <span class="font-weight-normal my-1">{{ name }}</span>
-    <br>
-    <span class="font-weight-light small">{{ content }}</span>
+    <div>
+        <span class="font-weight-normal my-1">{{ name }}</span>
+        <br>
+    <span class="font-weight-light small">{{ content }}</span>:&nbsp;<span class="font-weight-light small"><a v-if="type=='Column'" v-bind:href="'https://airtable.com/shrTR908BwRWJ2Zap?prefill_Status=Pending&prefill_Type=Column&prefill_Title%20of%20text=' + encodeURIComponent(content) + '&prefill_Contact%20Author=' + encodeURIComponent(name)">submission form</a><a v-else-if="type=='Forum'" v-bind:href="'https://airtable.com/shry0gQP1LpJrIth3?prefill_Status=Pending&prefill_Type=Forum&prefill_Title%20of%20text=' + encodeURIComponent(content) + '%3A%20&prefill_Sub%20Editor=' + encodeURIComponent(name)">submission form</a><a v-else href="https://airtable.com/shr0sfqAldfNfkIs7?prefill_Status=Pending">general form</a></span>
     </div>`
 })
 
