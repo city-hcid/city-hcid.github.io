7cb539149fbb2176665493482ef7cd942e7260a3
HCID <hcid@city.ac.uk>
2020-06-17 23:22:20 +0100

    update

--- js/vue_components.js
+++ js/vue_components.js
@@ -115,3 +115,8 @@ Vue.component('template-footer', {
     </footer>
     `
 })
+
+Vue.component('template-listing', {
+    props: ['theme', 'item', 'name', 'url'],
+    template: `<span v-if="item.includes(theme)"><a v-bind:href="url" target="_blank">{{ name }}</a>, </span>`
+})
