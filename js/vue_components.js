a2d89ad504f02b4fe45a85bad435aac7cf51ffaa
HCID <hcid@city.ac.uk>
2020-06-20 14:22:46 +0100

    update

--- js/vue_components.js
+++ js/vue_components.js
@@ -7,11 +7,11 @@ Vue.component('template-header', {
                 <div class="nav-wrapper white">
                     <a href="#" data-target="mobile-nav" class="sidenav-trigger hide-on-med-and-up"><i class="material-icons grey-text text-darken-2">menu</i></a>
                     <ul id="nav-mobile" class="left hide-on-small-and-down">
-                        <li class="active"><a class="dropdown-trigger grey-text text-darken-2" href="#!" data-target="dropdown1">HCID</a></li>
-                        <li><a href="../centre/charter" class="grey-text text-darken-2">Charter</a></li>
-                        <li><a href="../centre/members" class="grey-text text-darken-2">Members</a></li>
-                        <li><a href="../centre/research" class="grey-text text-darken-2">Research</a></li>
-                        <li><a href="../centre/projects" class="grey-text text-darken-2">Projects</a></li>
+                        <li v-bind:class="isActiveHCID ? 'active' : ''"><a class="dropdown-trigger grey-text text-darken-2" href="#!" data-target="dropdown1">HCID</a></li>
+                        <li v-bind:class="isActiveCharter ? 'active' : ''"><a href="../centre/charter" class="grey-text text-darken-2">Charter</a></li>
+                        <li v-bind:class="isActiveMembers ? 'active' : ''"><a href="../centre/members" class="grey-text text-darken-2">Members</a></li>
+                        <li v-bind:class="isActiveResearch ? 'active' : ''"><a href="../centre/research" class="grey-text text-darken-2">Research</a></li>
+                        <li v-bind:class="isActiveProjects ? 'active' : ''"><a href="../centre/projects" class="grey-text text-darken-2">Projects</a></li>
                     </ul>
                     <a href="https://www.city.ac.uk" class="brand-logo right" style="opacity: 0;" aria-label="Image link to City University website"><img class="hide-on-small-and-down" src="https://www.city.ac.uk/__data/assets/git_bridge/0018/344007/main/i/logo/city-uol-logo-responsive-125.svg" alt="City University logo"></a>
                 </div>
@@ -23,7 +23,7 @@ Vue.component('template-header', {
             <li v-bind:class="isActiveHCID ? 'active' : ''"><a class="menu-header" href="./">HCID</a></li>
             <li v-bind:class="isActiveCharter ? 'active' : ''"><a href="./centre/charter" class="menu-subheader">Charter</a></li>
             <li v-bind:class="isActiveMembers ? 'active' : ''"><a href="./centre/members" class="menu-subheader">Members</a></li>
-            <li v-bind:class="isActiveProjects ? 'active' : ''"><a href="./centre/research" class="menu-subheader">Research</a></li>
+            <li v-bind:class="isActiveResearch ? 'active' : ''"><a href="./centre/research" class="menu-subheader">Research</a></li>
             <li v-bind:class="isActiveProjects ? 'active' : ''"><a href="./centre/projects" class="menu-subheader">Projects</a></li>
             <li v-bind:class="isActiveCalendar ? 'active' : ''"><a href="./centre/calendar" class="menu-subheader">Calendar</a></li>
             <li class="divider" tabindex="-1"></li>
