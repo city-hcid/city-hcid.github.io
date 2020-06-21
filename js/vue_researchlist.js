3dc33bc41d6e04d3f8931664ac1c9df9a2de8f28
Alex <alex@rosemary.local>
2020-06-21 23:20:43 +0100

    update

--- js/vue_researchlist.js
+++ js/vue_researchlist.js
@@ -1,8 +1,4 @@
-Vue.component('template-listing', function(resolve, reject) {
-    setTimeout(function() {
-        resolve({
-            props: ['theme', 'item', 'name', 'url'],
-            template: `<span v-if="item.includes(theme)"><a v-bind:href="url" target="_blank">{{ name }}</a>, </span>`
-        })
-    }, 500)
+Vue.component('template-listing', {
+    props: ['theme', 'item', 'name', 'url'],
+    template: `<span v-if="item.includes(theme)"><a v-bind:href="url" target="_blank">{{ name }}</a>, </span>`
 })
