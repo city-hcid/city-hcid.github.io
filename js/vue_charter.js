95574a18cbc2b30f1a52091d3fe217ef55c6bdbd
Alex <alex@rosemary.local>
2020-06-30 22:16:03 +0100

    update

---
+++ js/vue_charter.js
@@ -0,0 +1,11 @@
+AOS.init();
+$(function() { // Shorthand for $( document ).ready()
+    $('.dropdown-trigger').dropdown({
+        hover: true
+    });
+    $('.tabs').tabs();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax()
+});
+var app_id = "appVrmQAGy96E1jEP";
+var app_key = "keyC83ksN49wS10kX";
