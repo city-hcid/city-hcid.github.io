c700dad92c74ae87b68b4e930368544a0bc3b092
HCID <hcid@city.ac.uk>
2020-07-13 23:16:53 +0100

    update

--- .netlify/functions/memberFunction/memberFunction.js
+++ .netlify/functions/memberFunction/memberFunction.js
@@ -1,7 +1,25 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const { AIRTABLE_ENDPOINT, HCID_ID, HCID_KEY } = process.env;
+    const Airtable = require('airtable');
+    var data = [];
+    await new Airtable({apiKey: HCID_KEY})
+      .base(HCID_ID)('members')
+      .select({
+        maxRecords: 3,
+        view: "Grid view"
+     }).firstPage((err, records) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        //all records are in the `records` array, do something with it
+        else {
+          console.log(records);
+          data = records
+        }
+    })
     return {
       statusCode: 200,
       headers: {
@@ -9,7 +27,7 @@ exports.handler = async (event, context) => {
         'Access-Control-Allow-Headers':
           'Origin, X-Requested-With, Content-Type, Accept',
       },
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify(data),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
