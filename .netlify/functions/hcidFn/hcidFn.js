e7c451bb05e8dbf7d36be0fac4db853ea7699708
HCID <hcid@city.ac.uk>
2020-07-18 14:59:57 +0100

    update

--- .netlify/functions/hcidFn/hcidFn.js
+++ .netlify/functions/hcidFn/hcidFn.js
@@ -35,6 +35,6 @@ exports.handler = function(event, context, callback) {
       for (i in records) {
           data.push(records[i].fields);
       }
-      send(data);//use records if you are using self.items = response.data.records and item['fields']...
+      send(data);//send records if you are using self.items = response.data.records and item['fields']...
    })
 }
