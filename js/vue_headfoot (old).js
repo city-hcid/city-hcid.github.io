1778836fc6ce4edfc3a44042a1bad3e1db4e9f92
alwaysalready <24851487+alwaysalready@users.noreply.github.com>
2022-02-07 10:45:55 +0000

    update

--- js/vue_headfoot (old).js
+++ js/vue_headfoot (old).js
@@ -72,6 +72,14 @@ Vue.component('template-header', {
                 :class="navLinkClass"
                 :title="item.name">{{ item.name }}</a>
             </li>
+            /* <li v-for="(item,index) in dropDownCentre" v-if="index>0" @click="active = !active" :class="active" 
+            :id="'sideNavItem' + item.name.replace(/ /g,'_')">
+                <a 
+                :href="item.url"
+                :class="navLinkClass"
+                :title="item.name">{{ item.name }}</a>
+            </li>
+            <li class="divider" tabindex="-1"></li> */
             <li v-for="(item, index) in dropDownSeminar" @click="active = !active" :class="active" :id="item.id">
                 <a v-if="index == 0"
                 :href="item.url"
@@ -95,6 +103,18 @@ Vue.component('template-header', {
                 :class="dropDownSubLinkClass"
                 :title="item.name">{{ item.name }}</a>
             </li>
+            /* <li class="divider" tabindex="-1"></li>
+            <li v-for="(item, index) in dropDownReading" @click="active = !active" :class="active" :id="item.id">
+                <a v-if="index == 0"
+                :href="item.url"
+                :class="dropDownLinkClass"
+                :title="item.name">{{ item.name }}</a>
+                <a v-else
+                :href="item.url"
+                :class="dropDownSubLinkClass"
+                :title="item.name">{{ item.name }}</a>
+            </li>
+            <li class="divider" tabindex="-1"></li> */
             <li v-for="(item, index) in dropDownSeminar" @click="active = !active" :class="active" :id="item.id">
                 <a v-if="index == 0"
                 :href="item.url"
