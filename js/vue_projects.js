160dec142fb7c8ec2db0f906bc74a1ac57bf1796
Alex Taylor <24851487+alwaysalready@users.noreply.github.com>
2022-06-15 13:39:18 +0100

    update

--- js/vue_projects.js
+++ js/vue_projects.js
@@ -41,7 +41,7 @@ var app = new Vue({
                     params: {
                         table: encodeURI('projects'),
                         view: encodeURI('archive'),
-                        fields: encodeURI('project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,members,members-str,lead-str,research-theme'),
+                        fields: encodeURI('project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme'),
                         sort: encodeURI('{"field":"project-name","direction":"asc"}')
                     }
                 })
