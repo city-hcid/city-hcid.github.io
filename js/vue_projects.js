48c967ee37a6b2f8a9f971ab6f1f6b0d8b75940b
Alex Taylor <24851487+alwaysalready@users.noreply.github.com>
2022-12-14 15:18:10 +0000

    update

--- js/vue_projects.js
+++ js/vue_projects.js
@@ -1,63 +1,74 @@
 AOS.init();
 
-$(function() {
-    $('.dropdown-trigger').dropdown({
-        hover: true
-    });
-    $('.tabs').tabs();
-    $('.sidenav').sidenav()
-})
+$(function () {
+	$(".dropdown-trigger").dropdown({
+		hover: true,
+	});
+	$(".tabs").tabs();
+	$(".sidenav").sidenav();
+});
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        liveItems: [],
-        archiveItems: []
-    },
-    mounted: function() {
-        this.loadItems();
-    },
-    methods: {
-        loadItems: function() {
-            let url = '';
-            if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "happy-galileo-a42c9d.netlify.app") {
-                url = '../.netlify/functions/hcidFn'
-            } else {
-                url = 'https://happy-galileo-a42c9d.netlify.app/.netlify/functions/hcidFn'
-            };
-            var self = this;
-            self.liveItems = [];
-            self.archiveItems = [];
-            axios.all([
-                axios.get(url, {
-                    params: {
-                        table: encodeURI('projects'),
-                        view: encodeURI('live'),
-                        fields: encodeURI('project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme'),
-                        sort: encodeURI('{"field":"project-name","direction":"asc"}')
-                    }
-                }),
-                axios.get(url, {
-                    params: {
-                        table: encodeURI('projects'),
-                        view: encodeURI('archive'),
-                        fields: encodeURI('project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme'),
-                        sort: encodeURI('{"field":"project-name","direction":"asc"}')
-                    }
-                })
-            ]).then(function(response) {
-                self.liveItems = response[0].data;
-                self.archiveItems = response[1].data;
-            }).catch(function(error) {
-                console.log(error)
-            })
-        }
-    }
-})
+	el: "#app",
+	data: {
+		liveItems: [],
+		archiveItems: [],
+	},
+	mounted: function () {
+		this.loadItems();
+	},
+	methods: {
+		loadItems: function () {
+			let url = "";
+			if (
+				location.hostname === "localhost" ||
+				location.hostname === "127.0.0.1" ||
+				location.hostname === "happy-galileo-a42c9d.netlify.app"
+			) {
+				url = "../.netlify/functions/hcidFn";
+			} else {
+				url = "https://happy-galileo-a42c9d.netlify.app/.netlify/functions/hcidFn";
+			}
+			var self = this;
+			self.liveItems = [];
+			self.archiveItems = [];
+			axios
+				.all([
+					axios.get(url, {
+						params: {
+							table: encodeURI("projects"),
+							view: encodeURI("live"),
+							fields: encodeURI(
+								"project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme"
+							),
+							sort: encodeURI('{"field":"project-name","direction":"asc"}'),
+						},
+					}),
+					axios.get(url, {
+						params: {
+							table: encodeURI("projects"),
+							view: encodeURI("archive"),
+							fields: encodeURI(
+								"project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme"
+							),
+							sort: encodeURI('{"field":"project-name","direction":"asc"}'),
+						},
+					}),
+				])
+				.then(function (response) {
+					self.liveItems = response[0].data;
+					self.archiveItems = response[1].data;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		},
+	},
+});
 
-Vue.component('template-project', {
-    props: ['item'],
-    template: `
+Vue.component("template-project", {
+	props: ["item"],
+	template: `
         <div v-bind:id="item['project-name']" v-if="item['project-name']" class="my-5">
             <a v-if="item['image-url']" v-bind:href="item['project-url']"><img class="responsive-img" v-bind:src="item['image-url']" v-bind:alt="item['project-name'] + ' icon'" style="max-height:8rem" /></a>
             <div>
@@ -68,7 +79,7 @@ Vue.component('template-project', {
                 <span v-if="item['long-name']" class="lbr">{{ item['long-name'] }}</span>
                 <p>
                     <strong>About: </strong>
-                    <span v-if="item['about']" class="lbr grey-text text-darken-2">{{ item['about'] }}</span>
+                    <span v-if="item['about']" class="lbr grey-text text-darken-2">{{ item['about'] }}</span> <span v-if="item['project-url']" class="lbr grey-text text-darken-2">See more <a href="{{ item['project-url'] }}" target="_blank">here</a>.</span>
                 </p>
                 <p class="pb-5">
                     <strong>Project members: </strong>
@@ -76,5 +87,5 @@ Vue.component('template-project', {
                 </p>
             </div>
         </div>
-    `
-})
+    `,
+});
