33f8b663edfb9375109b55d32a0d0518998dff65
Alex Taylor <24851487+alwaysalready@users.noreply.github.com>
2023-03-05 13:34:49 +0000

    token update

--- .netlify/functions/hcidRecFn/hcidRecFn.js
+++ .netlify/functions/hcidRecFn/hcidRecFn.js
@@ -1,41 +1,44 @@
-exports.handler = function(event, context, callback) {
-  const Airtable = require('airtable');
-  const { HCID_ID, HCID_KEY } = process.env;
-  const table = decodeURIComponent(event.queryStringParameters.table);
-  const filter = decodeURIComponent(event.queryStringParameters.filter);
-  const view = decodeURIComponent(event.queryStringParameters.view);
-  console.log(filter);
-  const fields = decodeURIComponent(event.queryStringParameters.fields).split(',');
-  
-  const send = body => {
-      callback(null, {
-          statusCode: 200,
-          headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Headers':
-              'Origin, X-Requested-With, Content-Type, Accept',
-          },
-          body: JSON.stringify( body )
-      });
-  }
+exports.handler = function (event, context, callback) {
+	const Airtable = require("airtable");
+	const { HCID_ID, hcid_personal_access_token } = process.env; //const { HCID_ID, HCID_KEY } = process.env;
+	const table = decodeURIComponent(event.queryStringParameters.table);
+	const filter = decodeURIComponent(event.queryStringParameters.filter);
+	const view = decodeURIComponent(event.queryStringParameters.view);
+	console.log(filter);
+	const fields = decodeURIComponent(event.queryStringParameters.fields).split(
+		","
+	);
 
-    new Airtable({apiKey: HCID_KEY})
-    .base(HCID_ID)(table)
-    .select({
-      maxRecords: 1,
-      view: view,
-      filterByFormula: filter,
-      fields: fields
-    }).firstPage((err, records) => {
-      if (err) {
-          console.error(err);
-          return
-      }
-      var data = [];
-      for (i in records) {
-          data.push(records[i].fields);
-      }
-      send(data);
-      console.log(data)
-   })
-}
+	const send = (body) => {
+		callback(null, {
+			statusCode: 200,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Headers":
+					"Origin, X-Requested-With, Content-Type, Accept",
+			},
+			body: JSON.stringify(body),
+		});
+	};
+
+	new Airtable({ apiKey: hcid_personal_access_token })
+		.base(HCID_ID)(table)
+		.select({
+			maxRecords: 1,
+			view: view,
+			filterByFormula: filter,
+			fields: fields,
+		})
+		.firstPage((err, records) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			var data = [];
+			for (i in records) {
+				data.push(records[i].fields);
+			}
+			send(data);
+			console.log(data);
+		});
+};
