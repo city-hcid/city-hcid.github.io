378eab1d0d89e72157612ad07bcdec9cd6d87b7e
Alex Taylor <24851487+alwaysalready@users.noreply.github.com>
2023-03-05 12:29:35 +0000

    update project page

--- js/vue_project_legacy.js
+++ js/vue_project_legacy.js
@@ -1,46 +1,65 @@
 AOS.init();
 
-$(function() {
-    $('.dropdown-trigger').dropdown({
-        hover: true
-    });
-    $('.tabs').tabs();
-    $('.sidenav').sidenav()
-})
+$(function () {
+	$(".dropdown-trigger").dropdown({
+		hover: true,
+	});
+	$(".tabs").tabs();
+	$(".sidenav").sidenav();
+});
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        items: []
-    },
-    mounted: function() {
-        this.loadItems();
-    },
-    methods: {
-        loadItems: function() {
-            var url_string = window.location.href;
-            var url = new URL(url_string);
-            var name = url.searchParams.get("name");
-            var self = this;
-            this.items = [];
-            axios.get(
-                "https://api.airtable.com/v0/" + app_id + "/projects?filterByFormula=(FIND(%22" + name + "%22%2C%7Bproject-name%7D))", {
-                    headers: {
-                        Authorization: "Bearer " + app_key
-                    }
-                }
-            ).then(function(response) {
-                self.items = response.data.records;
-            }).catch(function(error) {
-                console.log(error)
-            })
-        }
-    }
-})
+	el: "#app",
+	data: {
+		items: [],
+	},
+	computed: {
+		compiledMarkdown: function () {
+			return marked(this.markdown, { sanitize: true });
+		},
+	},
+	mounted: function () {
+		this.loadItems();
+	},
+	methods: {
+		loadItems: function () {
+			let name = new URL(window.location.href).searchParams.get("name");
+			let self = this;
+			this.items = [];
+			let url = "";
+			if (
+				location.hostname === "localhost" ||
+				location.hostname === "127.0.0.1" ||
+				location.hostname === "happy-galileo-a42c9d.netlify.app"
+			) {
+				url = "../.netlify/functions/hcidFn";
+			} else {
+				url = "https://happy-galileo-a42c9d.netlify.app/.netlify/functions/hcidFn";
+			}
+			axios
+				.get(url, {
+					params: {
+						table: encodeURI("projects"),
+						view: encodeURI("Grid view"),
+						filter: encodeURI('({project-name}="' + name + '")'),
+						fields: encodeURI(
+							"project-name,image-url,project-url,long-name,about,lead,lead-url,collaborators,collaborators-long,members,members-str,lead-str,research-theme,paper-doi,paper-citation"
+						),
+					},
+				})
+				.then(function (response) {
+					self.items = response.data;
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		},
+	},
+});
 
-Vue.component('template-project', {
-    props: ['item'],
-    template: `
+Vue.component("template-project", {
+	props: ["item"],
+	template: `
         <div v-bind:id="item['project-name']" v-if="item['project-name']" class="my-5">
             <a v-if="item['image-url']" v-bind:href="item['project-url']"><img class="responsive-img" v-bind:src="item['image-url']" v-bind:alt="item['project-name'] + ' icon'" style="max-height:8rem" /></a>
             <div>
@@ -61,5 +80,5 @@ Vue.component('template-project', {
                 </p>
             </div>
         </div>
-    `
-})
+    `,
+});
