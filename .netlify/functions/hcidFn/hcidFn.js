5de5d758a375da154c4fd556a5cf6ab28c44e48a
mztreee <treeemail@gmail.com>
2024-02-25 18:07:09 +0000

    revert

--- .netlify/functions/hcidFn/hcidFn.js
+++ .netlify/functions/hcidFn/hcidFn.js
@@ -36,11 +36,8 @@ exports.handler = function (event, context, callback) {
 				data.push(record.fields);
 			});
 			
-			console.log("fetch next page");
 			// Proceed to the next page
 			fetchNextPage();
-			console.log("fetch next page");
-
 	
 		}, function done(err) {
 			if (err) {
