402b7c9bc38770ac8cb4012f3a2cf95e76b8139f
mztreee <treeemail@gmail.com>
2024-02-25 17:41:18 +0000

    each

--- .netlify/functions/hcidFn/hcidFn.js
+++ .netlify/functions/hcidFn/hcidFn.js
@@ -30,17 +30,16 @@ exports.handler = function (event, context, callback) {
 			view: view,
 			fields: fields,
 			sort: sort,
-		})
-		.firstPage((err, records) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+		}).eachPage(function page(records) {
+    
 			var data = [];
 			for (i in records) {
 				data.push(records[i].fields);
 			}
 			send(data);
-			console.log(data); //send records if you are using self.items = response.data.records and item['fields']...
+			
+			fetchNextPage();
+		
 		});
+
 };
